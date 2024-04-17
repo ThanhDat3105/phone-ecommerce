5f02c8e7fc4066e0e88b6d28946bbffbe9142cff
thanhdat <caothanhdat02003@gmail.com>
2024-04-17 13:48:00 +0700

    updateUrlBe

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"VO3xIwYGuiVUqEDBQmJ5gEjyRBMAVLMMUqxPSW8WSgs=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"Y9BdplT/8X8YHQ2xKQhvejCzV/+v0Qy4NOjJeWecp9s=\"}"
